8d550ec4d4d80902cd8c0883fca35203f7c409b4
Danila Khodarev <66491707+Juggernauff@users.noreply.github.com>
2023-12-08 20:28:07 +0400

    Init project.

---
+++ MyGame/Game.cs
@@ -0,0 +1,103 @@
+﻿namespace MyGame
+{
+    class HangmanGame
+    {
+        private string _secretWord;
+        private List<char> _guessedLetters;
+        private int _maxAttempts;
+        private int _currentAttempts;
+
+        public HangmanGame(string word, int attempts)
+        {
+            _secretWord = word.ToUpper();
+            _maxAttempts = attempts;
+            _guessedLetters = new List<char>();
+            _currentAttempts = 0;
+        }
+
+        public void StartGame()
+        {
+            Console.WriteLine("Добро пожаловать в игру 'Виселица'!");
+            DisplayWord();
+
+            while (!IsGameOver())
+            {
+                char guess = GetPlayerGuess();
+                if (!ProcessGuess(guess))
+                {
+                    Console.WriteLine("Неверно! Осталось попыток: {0}", _maxAttempts - _currentAttempts);
+                }
+
+                DisplayWord();
+            }
+
+            if (IsWordGuessed())
+            {
+                Console.WriteLine("Поздравляю! Вы угадали слово!");
+            }
+            else
+            {
+                Console.WriteLine("Игра окончена. Загаданное слово: {0}", _secretWord);
+            }
+        }
+
+        private void DisplayWord()
+        {
+            foreach (char letter in _secretWord)
+            {
+                if (_guessedLetters.Contains(letter))
+                {
+                    Console.Write(letter + " ");
+                }
+                else
+                {
+                    Console.Write("_ ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private char GetPlayerGuess()
+        {
+            Console.Write("Введите букву: ");
+            char guess = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            return guess;
+        }
+
+        private bool ProcessGuess(char guess)
+        {
+            if (char.IsLetter(guess))
+            {
+                _guessedLetters.Add(guess);
+                if (!_secretWord.Contains(guess))
+                {
+                    _currentAttempts++;
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите букву.");
+                return false;
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            return IsWordGuessed() || _currentAttempts >= _maxAttempts;
+        }
+
+        private bool IsWordGuessed()
+        {
+            foreach (char letter in _secretWord)
+            {
+                if (!_guessedLetters.Contains(letter))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MyGame/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите секретное слово: ");
+            string secretWord = Console.ReadLine()!.ToLower();
+
+            Console.Write("Введите количество жизней: ");
+            int.TryParse(Console.ReadLine(), out int health);
+
+            Console.Clear();
+
+            HangmanGame hangmanGame = new HangmanGame(secretWord, health);
+            hangmanGame.StartGame();
+        }
+    }
+}
